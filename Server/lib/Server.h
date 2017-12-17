17916a8d30b07e4f6c2480a9a25fd0d4e8bd6970
Matt Jaeger <matthew.jaeger@gmail.com>
2017-12-17 14:24:36 -0800

    Add protobuf dependency (#3)

--- Server/lib/Server.h
+++ Server/lib/Server.h
@@ -1,3 +1,22 @@
 #pragma once
 
 #include <asio.hpp>
+#include "JaegerNet.pb.h"
+
+namespace JaegerNet
+{
+    class Server
+    {
+    public:
+        Server(asio::io_service& ioService, short port);
+
+        void do_receive();
+
+        void Send(std::size_t length);
+
+    private:
+        asio::ip::udp::socket m_socket;
+        asio::ip::udp::endpoint m_endpoint;
+        std::array<char, 508> m_data;
+    };
+}
