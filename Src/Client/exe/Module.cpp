277891d2b28309e64f0e48cf0c9afb32e04ad5e9
Matt Jaeger <matthew.jaeger@gmail.com>
2017-12-24 11:02:00 -0800

    cmake bin directory (#13)

--- Src/Client/exe/Module.cpp
+++ Src/Client/exe/Module.cpp
@@ -1,63 +1,8 @@
-#include "Client.h"
-#include "InputListener.h"
+#include "JaegerNetClient.h"
 
 using namespace JaegerNet;
-using namespace std;
-
-class Test : public IMessageHandler
-{
-public:
-    void OnMessageReceived(IClient* const sender, MessageReceivedEventArgs& eventArgs) noexcept
-    {
-        auto message = eventArgs.Message;
-        auto error = message.error();
-        (error);
-
-        if (message.has_createlobbyresponse())
-        {
-            auto createLobbyMessage = message.createlobbyresponse();
-            auto lobbyId = createLobbyMessage.lobbyid();
-
-            auto connectMessage = std::make_unique<ConnectRequest>();
-            connectMessage->set_lobbyid(lobbyId);
-
-            JaegerNetRequest response;
-            response.set_allocated_connectrequest(connectMessage.release());
-
-            sender->Send(response, [](const JaegerNetResponse& /*response*/)
-            {
-            });
-        }
-        else
-        {
-            auto connectMessage = message.connectresponse();
-            auto playerNum = connectMessage.playernumber();
-
-            (playerNum);
-        }
-    }
-};
 
 int main(int /*argc*/, char** /*argv*/)
 {
-    std::vector<std::unique_ptr<IMessageHandler>> messageHandlers;
-    messageHandlers.emplace_back(std::make_unique<Test>());
-
-    JaegerNet::CreateClient("127.0.0.1", "31337", std::move(messageHandlers));
-    auto client = JaegerNet::GetClient();
-
-    auto createLobbyMessage = std::make_unique<CreateLobbyRequest>();
-    JaegerNetRequest message;
-    message.set_allocated_createlobbyrequest(createLobbyMessage.release());
-
-    client->Send(message, [](const JaegerNetResponse& /*response*/)
-    {
-    });
-
-    JaegerNet::CreateInputListener();
-    JaegerNet::GetInputListener()->Start();
-
-    client->Run(false);
-
     return 0;
 }
