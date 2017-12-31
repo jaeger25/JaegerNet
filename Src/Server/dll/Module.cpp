91579a3ce90a257535eca326cd8d8b77311bad82
Matt Jaeger <matt.jaeger@outlook.com>
2017-12-31 01:00:40 -0800

    Server dll

---
+++ Src/Server/dll/Module.cpp
@@ -0,0 +1,6 @@
+#include "JaegerNetServer.h"
+#include "Session.h"
+
+using namespace JaegerNet;
+
+std::unique_ptr<Session> JaegerNetServerSession;
---
+++ Src/Server/inc/JaegerNetServer.h
@@ -0,0 +1,5 @@
+#pragma once
+#include <functional>
+#include "JaegerNetServer_Exports.h"
+#include "JaegerNet_Types.h"
+#include "ErrorCodes.h"
